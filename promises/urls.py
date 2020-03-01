e038df571e09d25a5ca2e4f03edeb6ab69bee9b3
Kazuki Sato <kazuki.sato.japan@gmail.com>
2020-03-01 14:57:21 +0900

    add list view

--- promises/urls.py
+++ promises/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
+    path('list/', views.PromiseList.as_view(), name='list'),
     # path('test/', views.promise_detail, name='promise_detail'),
     path('<int:promise_id>/', views.promise_detail, name='promise_detail'),
     path('promise/', views.make_promise, name='make_promise'),
